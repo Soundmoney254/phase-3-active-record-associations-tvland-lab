e82a1565350f2dd7a45784ff4ffada39010fe640
Samuel <samnmbuguah@gmail.com>
2023-06-06 06:41:33 +0300

    Complete

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  has_one :network
+
+  def actors_list
+    actors.map(&:full_name)
+  end
 end
