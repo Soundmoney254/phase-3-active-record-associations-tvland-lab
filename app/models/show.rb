f2ace192785e6a18901040421808a107b801f2b6
Samuel <samnmbuguah@gmail.com>
2023-06-07 08:58:08 +0300

    Completed Assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,8 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  
   has_many :characters
   has_many :actors, through: :characters
-  has_one :network
 
   def actors_list
     actors.map(&:full_name)
